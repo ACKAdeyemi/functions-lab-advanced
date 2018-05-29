f05660ff7cc7724ebf5652cd0aeec09820a8d48e
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2018-05-29 16:57:17 +0100

    Finished first 10 tasks

--- app.js
+++ app.js
@@ -24,21 +24,93 @@ function reverseString(str) {
 }
 
 // 6. Write a function called 'disemvowel' that returns the vowels from the string parameter
+function disemvowel (str) {
+  // var vowels = ['a', 'e', 'i', 'o', 'u'];
+  // return str.split('').filter(function(el) {
+  //   return vowels.indexOf(el.toLowerCase()) == -1;
+  // }).join('');
+  return str.replace(/[aeiou]/gi, "");
+}
 
 // 7. Write a function called 'removeOdd' that removes all ODD number from an array
+// function removeOdd(arr) {
+//   var out = 0;
+//   for (var i = 0; i < arr.length; i++) {
+//     if (! (arr[i] % 2)) {
+//         arr[out++] = arr[i];
+//     }
+//   }
+//   arr.length = out;
+//   return arr;
+// }
+// MIMI'S VERSION
+function removeOdd(arr) {
+  return arr.filter(function(val){
+    return val % 2 === 0;
+  })
+}
 
 // 8. Write a function called 'removeEven' that removes all EVEN number from an array
+// function removeEven(arr) {
+//   var out = 0;
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2) {
+//         arr[out++] = arr[i];
+//     }
+//   }
+//   arr.length = out;
+//   return arr;
+// }
+// JACK'S VERSION
+function removeEven(arr) {
+  return arr.filter(function(val){
+    return val % 2 === 1;
+  })
+}
+
 
 // 9. Write a function called 'longestString' that takes an array of strings and returns the string with the longest character length
+// function longestString (arr,longest) {
+//   var longest = arr.sort(function (a, b) { return b.length - a.length; })[0];
+//   return longest;
+// }
+// BEN'S VERSION
+// function longestString (arr) {
+//   var longestWord = ""; // empty string
+//
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i].length > longestWord.length) {
+//       longestWord = arr[i];
+//     }
+//   }
+//   return longestWord;
+// }
+// NICK'S VERSION
+function longestString (arr) {
+  var longestWord = arr.reduce((a, b) => { //arrow function and turnary operators
+    return b.length > a.length ? b : a; });
+  return longestWord;
+}
 
 // 10. Write a function called 'allElementsExceptFirstThree' that discards the first 3 elements of an array,
 // e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
+function allElementsExceptFirstThree (arr) {
+  for (var i = 0; i < 3; i++) {
+    arr = arr.splice(1);
+  }
+  return arr;
+}
+// JACK'S VERSION
+function allElementsExceptFirstThree (arr) {
+  return arr.splice(3, arr.length - 3);
+}
 
 //#### BONUS ####
 
 // 11. Write a function called 'convertArrayToAnObject' that turns an array (with an even number of elements) into a hash, by
 // pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 // {'a' => 'b', 'c' => 'd'}
+
 
 // 12. Write a function called 'fizzBuzz' that takes any number and returns a value based on these rules
 
